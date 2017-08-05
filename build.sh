866f48b910699c97e295f3ab19e8582c5828775f
Sulekha Kulkarni <sulekha@fir04.gtisc.gatech.edu>
2017-08-05 01:41:02 +0000

    fix

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ $HOSTNAME =~ "fir" ]]; then
-   make -j 8 CCINC="-I $HOME/Error-Ranking/chord-fork/libs/boost_1_61_0 -I include " CCLIB="-L $HOME/Error-Ranking/chord-fork/libs/boost_1_61_0/installdir/lib -L $HOME/Error-Ranking/chord-fork/libs/gmp-6.1.0/installdir/lib -L lib " HOSTNAME=$HOSTNAME
-   g++ -std=c++11 -O2 -march=native -Wall -Wextra -Werror -I ./include -I $HOME/Error-Ranking/chord-fork/libs/boost_1_61_0 -L $HOME/Error-Ranking/chord-fork/libs/gmp-6.1.0/installdir/lib -lgmp -lgmpxx wrapper.cpp ./lib/libdai.a -o wrapper
+   make -j 8 CCINC="-I $HOME/Error-Ranking/chord-fork/libs/boost_1_61_0 -I $HOME/Error-Ranking/chord-fork/libs/gmp-6.1.0 -I include " CCLIB="-L $HOME/Error-Ranking/chord-fork/libs/boost_1_61_0/installdir/lib -L $HOME/Error-Ranking/chord-fork/libs/gmp-6.1.0/installdir/lib -L lib " HOSTNAME=$HOSTNAME
+   g++ -std=c++11 -O2 -march=native -Wall -Wextra -Werror -I ./include -I $HOME/Error-Ranking/chord-fork/libs/boost_1_61_0 -I $HOME/Error-Ranking/chord-fork/libs/gmp-6.1.0 -L $HOME/Error-Ranking/chord-fork/libs/gmp-6.1.0/installdir/lib -lgmp -lgmpxx wrapper.cpp ./lib/libdai.a -o wrapper
 else
    make -j 8 && \
    g++ -std=c++11 -O2 -march=native -Wall -Wextra -Werror -I ./include -lgmp -lgmpxx wrapper.cpp ./lib/libdai.a -o wrapper
